b95c8ef60370762c8d5b94eb5f67519c5957b8f0
SaifulJnU <47039014+SaifulJnU@users.noreply.github.com>
2020-11-19 22:57:50 +0600

    after adding comment

--- 01 knapsack.cpp
+++ 01 knapsack.cpp
@@ -10,21 +10,15 @@ int knapSack(int W, int wt[], int val[], int n)
     if (n == 0 || W == 0)
         return 0;
 
-    if(t[n][W]!=-1)
+    if(t[n][W]!=-1)   // memorization base
         return t[n][W];
 
 
     if (wt[n - 1] > W)
         return t[n][W]=knapSack(W, wt, val, n - 1);
     else
-        return t[n][W]= max(
-            val[n - 1]
-                + knapSack(W - wt[n - 1],
-                           wt, val, n - 1),
-            knapSack(W, wt, val, n - 1));
+        return t[n][W]= max(val[n - 1] + knapSack(W - wt[n - 1], wt, val, n - 1), knapSack(W, wt, val, n - 1));
 }
-
-
 
 int main()
 {
