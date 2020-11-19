79258f19c2c3deaa3f6d934e03a51dabcf24ad89
SaifulJnU <47039014+SaifulJnU@users.noreply.github.com>
2020-11-20 00:04:18 +0600

    after adding comment

--- 01 Knapsack recursive.cpp
+++ 01 Knapsack recursive.cpp
@@ -6,7 +6,7 @@ int knapSack(int W, int wt[], int val[], int n)
 {
 
     // Base Case
-    if (n == 0 || W == 0)
+    if (n == 0 || W == 0) //n=0 mane kono item nai ar W=0 mane knapsack tite kono kicu neyar moto jaiga nai.
         return 0;
 
    //diagram condition
@@ -14,6 +14,7 @@ int knapSack(int W, int wt[], int val[], int n)
         return knapSack(W, wt, val, n - 1);  //exclude the item
     else
         return max(val[n - 1] + knapSack(W - wt[n - 1], wt, val, n - 1), knapSack(W, wt, val, n - 1)); //optimal one= max(include the item, exclude the item)
+               // jokhon item ta include kore kaj korbo tokhon val[n-1] add hobe ar W-wt[n-1](mane knapsack er capacity komaite hobe) add hobe.
 }
 
 int main()
