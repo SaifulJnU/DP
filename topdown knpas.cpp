759b34dbd540f8488862a628450c7f7fea8d6b73
Md. Saiful Islam <47039014+SaifulJnU@users.noreply.github.com>
2020-11-24 19:03:13 +0600

    add top-down approach of knapsack prob

---
+++ topdown knpas.cpp
@@ -0,0 +1,36 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int knapSack(int W, int wt[], int val[], int n)
+{
+    int i, j;
+    int T[n + 1][W + 1];
+
+    for (i = 0; i <= n; i++)
+    {
+        for (j = 0; j <= W; j++)
+        {
+            if (i == 0 || j == 0)
+                T[i][j] = 0;
+
+            else if (wt[i - 1] <= j)
+                T[i][j] = max(val[i - 1]+ T[i - 1][j- wt[i - 1]], T[i - 1][j]); ///ekhane knapsack function ke call na kore optimal
+                                                                                ///data T namok table store kore rakhlam.
+            else
+                T[i][j] = T[i - 1][j];
+        }
+    }
+
+    return T[n][W];
+}
+
+int main()
+{
+    int val[] = { 60, 100, 120 };
+    int wt[] = { 10, 20, 30 };
+    int W = 50;
+    int n = sizeof(val) / sizeof(val[0]);
+    printf("%d", knapSack(W, wt, val, n));
+    return 0;
+}
